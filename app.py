55cb64c217fdb44e23b835068b54f0a6465fba47
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2020-07-17 19:43:40 +0200

    Adding missing fields to Artist and Venue models.

--- app.py
+++ app.py
@@ -35,12 +35,16 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
+    genres = db.Column(db.String(120), nullable=True)
     city = db.Column(db.String(120), nullable=False)
     state = db.Column(db.String(120), nullable=False)
     address = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120), nullable=False)
     image_link = db.Column(db.String(500), nullable=True)
     facebook_link = db.Column(db.String(120), nullable=True)
+    website = db.Column(db.String(120), nullable=False)
+    seeking_talent = db.Column(db.Boolean, default=False, nullable=False)
+    seeking_description = db.Column(db.String(), nullable=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -55,6 +59,9 @@ class Artist(db.Model):
     genres = db.Column(db.String(120), nullable=True)
     image_link = db.Column(db.String(500), nullable=True)
     facebook_link = db.Column(db.String(120), nullable=True)
+    website = db.Column(db.String(120), nullable=False)
+    seeking_venue = db.Column(db.Boolean, default=False, nullable=False)
+    seeking_description = db.Column(db.String(), nullable=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
